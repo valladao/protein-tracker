2d02b4da306787ac3baef6878b7357f72bee3411
Manoel Valladão <valladao@gmail.com>
2025-07-31 11:51:33 -0300

    Setup Firebase Data Storage (#3)
    * Sets up React with Vite and TypeScript
    
    Initializes a React application using Vite and TypeScript.
    
    This commit sets up the basic project structure, including:
    - Configuration for ESLint with TypeScript support
    - An index HTML file
    - Source files for the React application (App component, main entry point)
    - Basic CSS styling
    - TypeScript configuration files
    - Vite configuration
    
    These changes provide a foundation for building a protein tracker application.
    
    * Configures Tailwind CSS for styling
    
    Sets up Tailwind CSS with Vite for styling the application.
    Adds necessary dependencies and configuration files.
    Removes default react logos and related code.
    
    * Removes default styling
    
    Removes the default CSS styling.
    
    This change allows for a cleaner, more flexible approach to styling.
    
    * Adds Firebase SDK for application features
    
    Adds the Firebase SDK as a dependency to enable integration with Firebase services.
    This allows the application to utilize features such as authentication, database,
    storage, and more, enhancing its functionality and scalability.
    
    * Initializes Firebase for data storage
    
    Sets up Firebase with project configuration.
    This provides the backend infrastructure to persist
    and retrieve data, enabling core application features.
    
    * Hide firebase data
    
    Move to .env file

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "firebase/database"
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
